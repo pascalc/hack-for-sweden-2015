26a61f0fe845a10b4a5f9fea32d8c17cf5eeaf53
Pascal Chatterjee <pascal.chatterjee@gmail.com>
2015-03-14 14:25:00 +0100

    populate jobCards with initial data from list query

--- src/app/controllers/StartCtrl.js
+++ src/app/controllers/StartCtrl.js
@@ -2,10 +2,13 @@
 
 angular.module('hack4sweden').controller("StartCtrl", function($scope, $rootScope, $log, JobResource) {
   $rootScope.showLoading(true);
-  $scope.state = {};
+  $scope.state = {
+    searchTerm: "social media"
+  };
   JobResource.getList('social+media', "1").then(function(response) {
     $rootScope.showLoading(false);
     $scope.state.data = response.data.matchningslista.matchningdata;
+    $log.log($scope.state.data[0]);
   });
   window.scope = $scope;
 });
